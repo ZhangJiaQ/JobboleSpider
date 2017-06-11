c7b73f843160050d2c0c9ddf80f0b4838cca9550
ZhangJiaQ <judgezhang123@163.com>
2017-06-11 15:06:27 +0800

    6th commit

--- spiders/jobbole.py
+++ spiders/jobbole.py
@@ -5,7 +5,9 @@ from urllib import parse
 import scrapy
 import re
 from scrapy.http import Request
-from ArticleSpider.items import JobboleArticleItem
+from scrapy.loader import ItemLoader
+
+from ArticleSpider.items import JobboleArticleItem,ArticleItemLoader
 from ArticleSpider.utills.common import get_md5
 
 class JobboleSpider(scrapy.Spider):
@@ -25,8 +27,6 @@ class JobboleSpider(scrapy.Spider):
             post_url = post_node.css("::attr(href)").extract_first("")
             yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url},callback=self.content_parse)
 
-
-
         # 提取下一页并交给scrapy进行下载
         next_url = response.css('.navigation .next ::attr(href)').extract_first("")
         if next_url:
@@ -35,55 +35,21 @@ class JobboleSpider(scrapy.Spider):
 
     def content_parse(self , response):
 
-        article_item = JobboleArticleItem()
-
         front_image_url = response.meta.get("front_image_url", "")# feng mian tu
 
-        title = response.css('#wrapper .entry-header h1::text').extract()[0]
+        # 通过item loader加载item
+        item_loader = ArticleItemLoader(item=JobboleArticleItem(),response=response)
+        item_loader.add_css("title",'#wrapper .entry-header h1::text')
+        item_loader.add_css("create_time",".grid-8 .entry-meta-hide-on-mobile ::text")
+        item_loader.add_css("parise_nums",".vote-post-up h10::text")
+        item_loader.add_css("comment_nums","a[href='#article-comment'] span::text")
+        item_loader.add_css("fav_nums",".bookmark-btn::text")
+        item_loader.add_xpath("tages",'//p[@class="entry-meta-hide-on-mobile"]/a/text()')
+        item_loader.add_xpath("content",'//div[@class="entry"]')
+        item_loader.add_value("url",response.url)
+        item_loader.add_value("url_object_id",get_md5(response.url))
+        item_loader.add_value("front_image_url",front_image_url)
 
-        create_date = response.css('.grid-8 .entry-meta-hide-on-mobile ::text').extract()[0].strip().replace("·","").strip()
-
-        parise_nums = int(response.css(".vote-post-up h10::text").extract()[0])
-
-        fav_nums = response.css(".bookmark-btn::text").extract()[0]
-        match_re = re.match(".*?(\d+).*", fav_nums)
-        if match_re:
-            fav_nums = int(match_re.group(1))
-        else:
-            fav_nums = 0
-
-        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
-        match_re = re.match(".*?(\d+).*", comment_nums)
-        if match_re:
-            comment_nums = int(match_re.group(1))
-        else:
-            comment_nums = 0
-
-        content = response.xpath('//div[@class="entry"]').extract()[0]
-
-        tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
-        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
-        tags = ",".join(tag_list)
-
-        """
-        front_image_path = scrapy.Field()
-        """
-        article_item["title"] = title
-        try:
-            create_date = datetime.datetime.strptime(create_date, "%Y:%m:%d").date()
-        except Exception as e:
-            create_date = datetime.datetime.now().date()
-
-
-        article_item["create_time"] = create_date
-        article_item["parise_nums"] = parise_nums
-        article_item["comment_nums"] = comment_nums
-        article_item["fav_nums"] = fav_nums
-        article_item["tages"] = tags
-        article_item["content"] = content
-        article_item["url"] = response.url
-        article_item["url_object_id"] = get_md5(response.url)
-        article_item["front_image_url"] = front_image_url
-
+        article_item = item_loader.load_item()
 
         yield article_item
